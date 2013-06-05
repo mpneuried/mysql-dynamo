5f8efd73965757a1a98af0f7237739c97c5b3dcb
Mathias Peter <mathiaspeter@Mathiass-iMac.local>
2013-06-05 14:55:10 +0200

    fixed travis.yml and added generated lib files

---
+++ index.js
@@ -0,0 +1,12 @@
+(function() {
+  exports.version = '0.1.0';
+
+  exports.getSqlBuilder = require('./lib/sql');
+
+  exports.utils = require('./lib/utils');
+
+  exports.utils = require('./lib/table');
+
+  module.exports = require('./lib/manager');
+
+}).call(this);
