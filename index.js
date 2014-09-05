725d9d8b112bed30f063dc5faf4f6ca801bfb037
Mathias Peter <mathiaspeter@Mathiass-iMac.local>
2014-09-05 16:07:41 +0200

    compiled

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 (function() {
-<<<<<<< HEAD
-  exports.version = '0.3.0';
-=======
-  exports.version = '@@version';
->>>>>>> eaa0e8f0da9d0dbdbbc83d6d0ef9a8026c70cae6
+  exports.version = '0.3.2';
 
   exports.getSqlBuilder = require('./lib/sql');
 
